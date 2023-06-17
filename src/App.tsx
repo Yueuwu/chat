403f48f9012c71b055848deeec1f5472904f2ee5
Yueuwu <vladdenega1@gmail.com>
2023-06-17 14:59:48 +0700

    added raw firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
 import React from "react"
 import { useAppDispatch, useAppSelector } from "./app/hooks"
-import {SignIn, SignOut, auth, authSelector} from './redux/AuthSlice'
+import { SignIn, SignOut, authSelector } from "./redux/AuthSlice"
 import { useAuthState } from "react-firebase-hooks/auth"
-
+import { useCollectionData } from "react-firebase-hooks/firestore"
+import { auth, firestore } from "./firebase"
+import { collection, getDocs, addDoc } from "firebase/firestore"
 
 const App: React.FC = () => {
 	const dispatch = useAppDispatch()
-	const {isAuth} = useAppSelector(authSelector)
+	const { isAuth } = useAppSelector(authSelector)
 	const [user, loading] = useAuthState(auth)
-    console.log(!loading && user?.displayName)
-	
+	console.log(!loading && user?.displayName)
+
+	const f = async () => {
+		const querySnapshot = await getDocs(collection(firestore, "users"))
+		querySnapshot.forEach((doc) => {
+			console.log(`${doc.id} => ${doc.data()}`)
+		})
+	}
+
+	const s = async () => {
+		try {
+			const docRef = await addDoc(collection(firestore, "users"), {
+				first: "Ada",
+				last: "Lovelace",
+				born: 1815,
+			})
+			console.log("Document written with ID: ", docRef.id)
+		} catch (e) {
+			console.error("Error adding document: ", e)
+		}
+	}
+
 	return (
-	<div>
-		<button onClick={() => dispatch(SignIn())}>SignIn</button>
-		<button onClick={() => dispatch(SignOut())}>SignOut</button>
-		<>name: {user?.displayName}</>
-	</div>
-)}
+		<div>
+			<button onClick={() => dispatch(SignIn())}>SignIn</button>
+			<button onClick={() => dispatch(SignOut())}>SignOut</button>
+			<>name: {user?.displayName}</>
+			<button onClick={f}>Read</button>
+			<button onClick={s}>Send</button>
+		</div>
+	)
+}
 
 export default App
--- src/redux/AuthSlice.ts
+++ src/redux/AuthSlice.ts
@@ -1,20 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { RootState } from "../app/store"
-import { initializeApp } from "firebase/app"
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
-
-const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-}
-const app = initializeApp(firebaseConfig)
-
-const provider = new GoogleAuthProvider()
-export const auth = getAuth(app)
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
+import {auth, provider} from '../firebase'
 
 interface stateI {
     isAuth: boolean
