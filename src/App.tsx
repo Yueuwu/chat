30c3c66c2d0015bfbbb064f77dc1adfb4c8bb45e
Yueuwu <vladdenega1@gmail.com>
2023-06-19 20:05:37 +0700

    fix firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,52 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "./app/hooks"
 import { SignIn, SignOut, authSelector } from "./redux/AuthSlice"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { useCollection } from "react-firebase-hooks/firestore"
 import { auth, firestore } from "./firebase"
 import { collection } from "firebase/firestore"
-import { sendMessage } from "./redux/StoreSlice"
+import { sendMessage, storeSelector, changeMessage, setUser } from "./redux/StoreSlice"
 
 const App: React.FC = () => {
 	const dispatch = useAppDispatch()
+
 	const { isAuth } = useAppSelector(authSelector)
+	const { message } = useAppSelector(storeSelector)
 	const [user, loading] = useAuthState(auth)
-	
-	const [value, load, error] = useCollection(collection(firestore, "users"), {
+
+	const [value, load, error] = useCollection(collection(firestore, "messages"), {
 		snapshotListenOptions: { includeMetadataChanges: true },
 	})
 
-	value?.docs.map((doc) => {
-		console.log(JSON.stringify(doc.data()))
-	})
+	useEffect(() => {
+		if (user){
+			const userState = {
+				userName: user.displayName,
+				userPhoto: user.photoURL,
+				userId: user.uid
+			}
+			dispatch(setUser(userState))
+		} else if (!loading) {
+			alert("U have to sign in")
+		}
+	}, [user])
+
+	const writeMessage = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		dispatch(changeMessage(event.target.value))
+	}
 
 	return (
 		<div>
 			<button onClick={() => dispatch(SignIn())}>SignIn</button>
 			<button onClick={() => dispatch(SignOut())}>SignOut</button>
 			<>name: {user?.displayName}</>
-			<button onClick={() => dispatch(sendMessage())}>Send</button>
+			<textarea value={message} onChange={(e) => writeMessage(e)}></textarea>
+			<button onClick={() => user && dispatch(sendMessage())}>Send</button>
+			<>
+				{!load && value?.docs.map((d, i) => {
+					return <li key={i}>`${JSON.stringify(d.data())}`</li>
+				})}
+			</>
 		</div>
 	)
 }
--- src/redux/StoreSlice.ts
+++ src/redux/StoreSlice.ts
@@ -1,25 +1,39 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
+import { RootState } from "../app/store"
 import { firestore } from "../firebase"
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 
 const initialState = {
 	message: "",
+	userName: "",
+	userPhoto: "",
+	userId: "",
 }
 
 export const storeSlice = createSlice({
 	name: "firestore",
 	initialState,
 	reducers: {
-		sendMessage: state => {
-			addDoc(collection(firestore, "users"), {
-				first: "AdaN",
-				last: "Lovelace",
-				born: 1815,
+		sendMessage: (state) => {
+			addDoc(collection(firestore, "messages"), {
+				message: state.message,
+				userName: state.userName,
+				userPhoto: state.userPhoto,
+				userId: state.userId,
+				createdAt: serverTimestamp(),
 			})
+		},
+		changeMessage: (state, action) => {
+			state.message = action.payload
+		},
+		setUser: (state, action) => {
+			state.userName = action.payload.userName
+			state.userPhoto = action.payload.userPhoto
+			state.userId = action.payload.userId
 		},
 	},
 })
 
-export const { sendMessage } = storeSlice.actions
-
+export const storeSelector = (state: RootState) => state.firestore
+export const { sendMessage, changeMessage, setUser } = storeSlice.actions
 export default storeSlice.reducer
