b981cbfcb37d1e696d68a3ffd2eb7cf79303d264
Yueuwu <vladdenega1@gmail.com>
2023-06-26 12:44:15 +0700

    made up dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,71 +1,67 @@
-import { Box, Divider, Grid } from "@mui/material"
+import { Box, Divider, Grid, CircularProgress } from "@mui/material"
 import { DocumentData, collection, orderBy, query } from "firebase/firestore"
 import React from "react"
-import { useCollection, useCollectionData } from "react-firebase-hooks/firestore"
+import {
+	useCollection,
+	useCollectionData,
+} from "react-firebase-hooks/firestore"
 import { firestore } from "../../firebase"
 import { User } from "firebase/auth"
-import style from './dialog.module.css'
+import style from "./dialog.module.css"
 import { messageI } from "../../redux/StoreSlice"
 import DialogItem from "../DialogItem/DialogItem"
 
 interface fullmessageI extends messageI {
-	createdAt: {nanoseconds: string, seconds: string}
+	createdAt: { nanoseconds: string; seconds: string }
 }
 export interface propsI {
 	data: fullmessageI
-    isUser: boolean
+	isUser: boolean
 }
 
 const Dialog: React.FC<User> = (user) => {
-
 	const [value, loading, error] = useCollection(
-        query(
-            collection(firestore, "messages"),
-            orderBy('createdAt')
-            )
+		query(collection(firestore, "messages"), orderBy("createdAt"))
 	)
-	
+
 	const propsMaker = (data: DocumentData): propsI => {
 		const propsData: fullmessageI = {
 			message: data.message,
 			userName: data.userName,
 			userId: data.userId,
 			userPhoto: data.userPhoto,
-			createdAt: data.createdAt
+			createdAt: data.createdAt,
 		}
 		const isUser = data.userId === user.uid
-		return {data: propsData, isUser: isUser}
+		return { data: propsData, isUser: isUser }
 	}
 
 	return (
 		<Box sx={{ display: "flex", justifyContent: "center" }}>
 			<div className={style.Box}>
-				<Grid
-					container
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						width: "100%",
-					}}
-				>
-					{value?.docs.map((doc, index) => (
-						<Grid key={index} xs={12}>
-							<DialogItem {...propsMaker(doc.data())}/>
-                            {/* {
-                                <div style={{
-                                    padding: '1vmin',
-                                    width: 'fit-content',
-                                    marginLeft: doc.data().userId === user.uid ? 'auto' : '',
-                                    
-                                }}>
-                                    {JSON.stringify(doc.data().message)}
-                                </div>
-                            } */}
-                            <Divider/>
+				{loading ? (
+					<div className={style.loader}>
+						<CircularProgress color="success" />
+					</div>
+				) : (
+					<>
+						<Grid
+							container
+							sx={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								width: "100%",
+							}}
+						>
+							{value?.docs.map((doc, index) => (
+								<Grid key={index} xs={12}>
+									<DialogItem {...propsMaker(doc.data())}/>
+								</Grid>
+							))}
 						</Grid>
-					))}
-				</Grid>
+					</>
+				)}
 			</div>
 		</Box>
 	)
--- src/components/DialogItem/DialogItem.tsx
+++ src/components/DialogItem/DialogItem.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { propsI } from "../Dialog/Dialog"
 import style from "./item.module.css"
 import { Avatar } from "@mui/material"
@@ -6,16 +6,41 @@ import { Avatar } from "@mui/material"
 const DialogItem: React.FC<propsI> = ({ data, isUser }) => {
 	const styleHandler = () => {
 		return {
-			padding: "1vmin",
 			width: "fit-content",
 			marginLeft: isUser ? "auto" : "",
 		}
 	}
 
+	const date = new Date(
+		Number(data.createdAt?.seconds + "000")
+	)?.toLocaleDateString()
+
 	return (
 		<div style={styleHandler()} className={style.wrapper}>
-            <Avatar sx={{maxWidth: '5vmin', maxHeight: '5vmin'}} src={data.userPhoto}/>
-			 {data.message}
+			<div
+				style={{
+					flexDirection: isUser ? "row-reverse" : "row",
+					justifyContent: isUser ? "end" : "start",
+				}}
+				className={style.top}
+			>
+				<Avatar
+					sx={{
+						maxWidth: "4vmin",
+						maxHeight: "4vmin",
+						margin: "0 1vmin",
+					}}
+					src={data.userPhoto}
+				/>
+				<br />
+				<h5>{data.userName}</h5>
+			</div>
+			<div className={style.mid}>
+				<p>{data.message}</p>
+			</div>
+			<div className={style.bottom}>
+				<p>{date}</p>
+			</div>
 		</div>
 	)
 }
